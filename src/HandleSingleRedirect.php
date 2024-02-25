91e58ff7f282f6cfefa5aa9cc377182f0c819e99
Stephen Perelson <stephenp@ringier.co.za>
2024-02-25 16:35:48 +0200

    Fix spelling and tweak to return the Response instead of creating a new redirect

--- src/HandleSingleRedirect.php
+++ src/HandleSingleRedirect.php
@@ -22,10 +22,11 @@ class HandleSingleRedirect
     {
         $url = '';
         $infiniteLoop = 0;
-        $response = $next($request);
-        $responseStatus = $response->status();
+        $originalResponse = $next($request);
+        $responseStatus = $originalResponse->status();
         $method = $request->method();
         $baseUrl = url('/');
+        $previousResponse = null;
 
         if (
             $method !== 'GET'
@@ -35,21 +36,22 @@ class HandleSingleRedirect
                 || $responseStatus >= 400
             )
         ) {
-            return $response;
+            return $originalResponse;
         }
 
         $cookies = $request->cookies->all();
         $server = $request->server->all();
-        $newReponse = $response;
+        $newResponse = $originalResponse;
         $allowedRedirectCount = config('single-redirect.redirect-count');
         $methodToUse = config('single-redirect.use-request-method', false) ? $method : 'HEAD';
 
-        while ($newReponse instanceof RedirectResponse) {
-            $url = $newReponse->headers->get('location');
+        while ($newResponse instanceof RedirectResponse) {
+            $url = $newResponse->headers->get('location');
+            $previousResponse = $newResponse;
 
             // If the redirect is leaving our domain, proceed with the redirect
             if (str_starts_with($url, $baseUrl) === false) {
-                return $newReponse;
+                return $newResponse;
             }
 
             if ($infiniteLoop++ > $allowedRedirectCount) {
@@ -76,19 +78,17 @@ class HandleSingleRedirect
                 }
             }
 
-            $newReponse = $kernel->handle($request2);
+            $newResponse = $kernel->handle($request2);
 
-            if ($newReponse->headers->get('location') == $url) {
+            if ($newResponse->headers->get('location') == $url) {
                 throw new \Exception("Redirecting to the same url!");
             }
         }
 
-        if (!empty($url)) {
-            return redirect($url, $responseStatus, [
-                'single-redirect' => $infiniteLoop,
-            ]);
+        if (!is_null($previousResponse)) {
+            return $previousResponse->header('single-redirect', $infiniteLoop);
         }
 
-        return $response;
+        return $originalResponse;
     }
 }
